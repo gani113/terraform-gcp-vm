10886f077f39e759947e433725c919e48f3b5e57
gdurvasula <gdurvasula@gmail.com>
2025-09-01 11:53:00 +0530

    added main and varaible files

---
+++ main.tf
@@ -0,0 +1,35 @@
+
+data "google_service_account" "gcp_cicd_sa" {
+    account_id = "gcp-cicd-sa"
+  
+}
+
+data "google_compute_network" "project_vpc" {
+    name = "project-vpc"
+    project = var.project
+  
+}
+
+# resource "google_service_account" "gcp_cicd_sa" {
+#     account_id = "gcp-cicd-sa"
+#     display_name = "gcp-cicd-sa"  
+# }
+
+resource "google_compute_instance" "jenkins_vm" {
+    name = "jenkins-vm"
+    machine_type = "e2-medium"
+    
+    boot_disk {
+      initialize_params {
+        image = "ubuntu-os-cloud/ubuntu-2404-lts-arm64"
+        size= 20
+      }
+    }
+
+    network_interface {
+      network = google_compute_network.project_vpc.self_link
+      subnetwork = google_compute_subnetwork.project-vpc-subnet.self_link
+
+      access_config {}
+    }
+}
---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "project" {
+    type = string
+    description = "This is project"
+    default = "binge-projects"
+}
